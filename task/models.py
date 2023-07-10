535357946db7738802e0bb9fc4ac6c2f0f9e3c08
Abram <israelvictory87@gmail.com>
2023-07-10 12:43:19 +0100

    Added docstrings to todo model

--- task/models.py
+++ task/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class Todo(models.Model):
+    """Database schema for todos table."""
+
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=600)
     completed = models.BooleanField(default=False)
