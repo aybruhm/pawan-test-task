2a857aed56340502c3b2e6b40eb43982921216fb
Abram <israelvictory87@gmail.com>
2023-07-10 12:57:07 +0100

    Added user column to todos table

--- task/models.py
+++ task/models.py
@@ -1,5 +1,6 @@
 # Django Imports
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Todo(models.Model):
@@ -7,6 +8,7 @@ class Todo(models.Model):
 
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=600)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
