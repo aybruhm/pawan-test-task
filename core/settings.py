af87f0d423f794836f0edbffaf5be2b5bdf796e3
Abram <israelvictory87@gmail.com>
2023-07-10 11:11:00 +0100

    Regitstered task app to core project

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b2wk0$wk40f+4054z%b=x^hp3y7tx5u$)+g*9%93n$aq7+qq@q"
+SECRET_KEY = (
+    "django-insecure-b2wk0$wk40f+4054z%b=x^hp3y7tx5u$)+g*9%93n$aq7+qq@q"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # local apps
+    "task.apps.TaskConfig",
 ]
 
 MIDDLEWARE = [
